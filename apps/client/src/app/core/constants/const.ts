a1bbfc46ad6f2168a0acd2fc5936b3415ab7537b
Sergey Malyuga <sergey@malyuga.ru>
2026-09-16 13:55:10 +0300

    feat(core): добавить enum для секций

---
+++ apps/client/src/app/core/constants/const.ts
@@ -0,0 +1,11 @@
+export enum AppRoute {
+  MAIN = ''
+}
+
+export enum Section {
+  MAIN = 'main',
+  CATALOG = 'catalog',
+  COLLECTION = 'collection',
+  COMMUNITY = 'community',
+  REVIEWS = 'reviews',
+}
