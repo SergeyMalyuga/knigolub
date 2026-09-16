a886f2b49989d3a88b40ee5e1d87e49dc4ac2122
Sergey Malyuga <sergey@malyuga.ru>
2026-09-16 13:57:27 +0300

    feat(routes): добавить маршрутизацию для главной страницы

--- apps/client/src/app/app.routes.ts
+++ apps/client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AppRoute} from './core/constants/const';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: AppRoute.MAIN,
+    title: 'Главная страница',
+    loadComponent: () => import('./pages/main/main.component').then(m => m.MainComponent),
+  }
+];
