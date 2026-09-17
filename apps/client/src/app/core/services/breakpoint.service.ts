01aad3b397bdb66e0c8cbe724d9c251349827dec
Sergey Malyuga <sergey@malyuga.ru>
2026-09-17 14:27:00 +0300

    feat(breackpoint): добавить сервис разрешения экрана

---
+++ apps/client/src/app/core/services/breakpoint.service.ts
@@ -0,0 +1,35 @@
+import {DOCUMENT, inject, Injectable, PLATFORM_ID, signal, WritableSignal} from '@angular/core';
+import {isPlatformBrowser} from '@angular/common';
+import {BREAKPOINTS} from '../constants/const';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BreakpointService {
+  private document = inject(DOCUMENT);
+  private platformId = inject(PLATFORM_ID);
+
+  private readonly tabletQuery?: MediaQueryList;
+  private readonly mobileQuery?: MediaQueryList;
+
+  public readonly isTablet = signal<boolean>(false);
+  public readonly isMobile = signal<boolean>(false);
+
+
+  public constructor() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.tabletQuery = this.document.defaultView?.matchMedia(BREAKPOINTS.TABLET);
+      this.mobileQuery = this.document.defaultView?.matchMedia(BREAKPOINTS.MOBILE);
+        this.listen(this.tabletQuery, this.isTablet);
+        this.listen(this.mobileQuery, this.isMobile);
+    }
+  }
+
+  private listen(query: MediaQueryList | undefined, signal: WritableSignal<boolean>) {
+    if (query) {
+      signal.set(query.matches);
+      const handler = (evt: MediaQueryListEvent) => signal.set(evt.matches);
+      query.addEventListener('change', handler);
+    }
+  }
+}
