6ad93a9ca9dafe116ff70d81cafd87ff5a61ab65
Sergey Malyuga <sergey@malyuga.ru>
2026-09-18 09:36:37 +0300

    feat(breakponit): добавить отслеживание разрешения 680px

--- apps/client/src/app/core/services/breakpoint.service.ts
+++ apps/client/src/app/core/services/breakpoint.service.ts
@@ -11,17 +11,21 @@ export class BreakpointService {
 
   private readonly tabletQuery?: MediaQueryList;
   private readonly mobileQuery?: MediaQueryList;
+  private readonly miniMobileQuery?: MediaQueryList;
 
   public readonly isTablet = signal<boolean>(false);
   public readonly isMobile = signal<boolean>(false);
+  public readonly isMiniMobile = signal<boolean>(false);
 
 
   public constructor() {
     if (isPlatformBrowser(this.platformId)) {
       this.tabletQuery = this.document.defaultView?.matchMedia(BREAKPOINTS.TABLET);
       this.mobileQuery = this.document.defaultView?.matchMedia(BREAKPOINTS.MOBILE);
+      this.miniMobileQuery = this.document.defaultView?.matchMedia(BREAKPOINTS.MINI_MOBILE);
         this.listen(this.tabletQuery, this.isTablet);
         this.listen(this.mobileQuery, this.isMobile);
+        this.listen(this.miniMobileQuery, this.isMiniMobile);
     }
   }
 
