570e8acc575f9b1d26fa3b3ea37746bfb90fbc46
Sergey Malyuga <sergey@malyuga.ru>
2026-09-18 09:37:21 +0300

    feat(header): скрыть меню от скринридеров при разрешении 680px

--- apps/client/src/app/shared/components/header/header.component.ts
+++ apps/client/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, computed, inject, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, effect, inject, signal} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {AppRoute, Section} from '../../../core/constants/const';
 import {NgClass} from '@angular/common';
@@ -20,9 +20,20 @@ export class HeaderComponent {
   protected readonly AppRoute = AppRoute;
   protected readonly Section = Section;
 
-  public isTablet = computed(() => this.breakpointService.isTablet());
   public isMobile = computed(() => this.breakpointService.isMobile());
   public activeSection = signal<Section>(Section.MAIN);
+  public isMenuOpen = signal<boolean>(true);
+
+  constructor() {
+    effect(() => {
+      const miniMobile = this.breakpointService.isMiniMobile();
+      if (miniMobile) {
+        this.isMenuOpen.set(false)
+      } else {
+        this.isMenuOpen.set(true)
+      }
+    });
+  }
 
   public isActive(section: Section): boolean {
     return this.activeSection() === section;
