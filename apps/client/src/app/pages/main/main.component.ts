eff57105420178786a6f7bd7535f9a218de16ba7
Sergey Malyuga <sergey@malyuga.ru>
2026-09-18 12:44:28 +0300

    feat(main): подключить секцию hero

--- apps/client/src/app/pages/main/main.component.ts
+++ apps/client/src/app/pages/main/main.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {HeaderComponent} from '../../shared/components/header/header.component';
+import {HeroComponents} from './components/hero/hero.components';
 
 @Component({
   selector: 'app-main',
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    HeroComponents
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
