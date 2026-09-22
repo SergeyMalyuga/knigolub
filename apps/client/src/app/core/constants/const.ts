16f3f123de04016c31ffa2a17aadb22f248c9a6f
Sergey Malyuga <sergey@malyuga.ru>
2026-09-22 12:46:55 +0300

    feat(core): добавить enum категорий книг

--- apps/client/src/app/core/constants/const.ts
+++ apps/client/src/app/core/constants/const.ts
@@ -1,3 +1,5 @@
+import {CategoryKey} from '../types/category-key';
+
 export enum AppRoute {
   MAIN = ''
 }
@@ -15,3 +17,25 @@ export const BREAKPOINTS = {
   MOBILE: '(max-width: 855px)',
   MINI_MOBILE: '(max-width: 680px)',
 } as const;
+
+export enum CategoryType {
+  ALL = 'all',
+  PROSE = 'prose',
+  NON_FICTION = 'non‑fiction',
+  FANTASTIC = 'fantastic',
+  DETECTIVES = 'detectives',
+  CLASSIC = 'classic',
+  POETRY = 'poetry',
+  CHILDREN = 'children'
+}
+
+export const CATEGORY_LABELS: Record<CategoryKey, string> = {
+  [CategoryType.ALL]: 'Все книги',
+  [CategoryType.PROSE]: 'Современная проза',
+  [CategoryType.NON_FICTION]: 'Нон-фикшн',
+  [CategoryType.FANTASTIC]: 'Фантастика',
+  [CategoryType.DETECTIVES]: 'Детективы',
+  [CategoryType.CLASSIC]: 'Классика',
+  [CategoryType.POETRY]: 'Поэзия',
+  [CategoryType.CHILDREN]: 'Детям',
+}
