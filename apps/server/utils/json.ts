f04d97b58d355493fb586384bacbf5a70b55c781
Sergey Malyuga <sergey@malyuga.ru>
2026-09-24 16:48:03 +0300

    feat(utils): добавить метод для преобразования json в массив

---
+++ apps/server/utils/json.ts
@@ -0,0 +1,8 @@
+import type {JsonValue} from "../generated/prisma/internal/prismaNamespace";
+
+export function jsonToStringArray(value: JsonValue): string[] {
+    if (Array.isArray(value)) {
+        return value.filter((item): item is string => typeof item === 'string');
+    }
+    return [];
+}
