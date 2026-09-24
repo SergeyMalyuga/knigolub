b0e016853fc56493d8621c4a22fc6ae2fc960fb1
Sergey Malyuga <sergey@malyuga.ru>
2026-09-24 16:37:02 +0300

    feat(routes): добавить маппинг для данных из БД

--- apps/server/routes/book.ts
+++ apps/server/routes/book.ts
@@ -1,11 +1,13 @@
 import {Router} from 'express';
 import {prisma} from "../lib/prisma";
+import {mapPrismaBookToShared} from "../mapper/book.mapper";
 
 const router = Router();
 
 router.get('/', async (req, res) => {
     try {
-        const books = await prisma.book.findMany();
+        const prismaBooks = await prisma.book.findMany();
+        const books = prismaBooks.map(book => mapPrismaBookToShared(book));
         res.json(books);
     } catch (err) {
         console.error('Ошибка при получении книг:', err);
